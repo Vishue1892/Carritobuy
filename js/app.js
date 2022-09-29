afe352f6d386be218c3539a6c9fc27f90b302176
Vushue1892 <victorsanchesbarrietos1203@gmail.com>
2022-09-29 13:14:23 -0500

    Leer los datos del curso que seleccionamos

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
-//variables
 const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
 
-cargarEventListeners();
-function cargarEventListeners(){
-    //
-    listaCursos.addEventListener('click', agregarCurso);
+cargarEventListener();
+function cargarEventListener(){
+    listaCursos.addEventListener('click', agregarCurso); 
 }
 
-// funciones
+
+//Funciones
 function agregarCurso(e){
+    e.preventDefault();
+
     if(e.target.classList.contains('agregar-carrito')){
-    console.log('Presionaste el boton');
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatoscurso(cursoSeleccionado);
+
     }
 }
+    //leer el contenido del HTML al que le dimos click y ectrae la informacon del curso
+function leerDatoscurso(curso){
+        console.log(curso);
+
+      //crear un objeto con el cotenido del curso actual
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            titulo: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('.precio span').textContent,
+            id: curso.querySelector('a').getAttribute('data-id'),
+     
+        }
+        console.log(infoCurso)
+    }
